129d7ec6636fcf2b121c90a151ac19c2d78c0f46
pouya <pouya.May4@gmail.com>
2025-04-14 18:29:02 +0330

    Project completed

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,21 +1,110 @@
+"use client";
+
+import React from "react";
 import blogsData from "../../data/blogs.json";
 
 export default function BlogPage({ params }) {
   const { slug } = params;
   const id = slug;
   const blog = blogsData.find((blog) => blog.id == parseInt(id));
-  console.log(id);
-  console.log(blog);
-  console.log(slug);
 
   if (!blog) {
     return <div> Blog not found</div>;
   }
+  const [commentDisplay, setCommentDisplay] = React.useState(true);
+  console.log(commentDisplay);
+  function handelComment() {
+    setCommentDisplay(!commentDisplay);
+  }
 
   return (
-    <div>
-      <h1> blog name: {blog.name}</h1>
-      <p>description: {blog.description}</p>
+    <div className="  px-6 py-3 flex flex-col min-h-screen justify-between ">
+      <div className="blogDescrip">
+        <h1 className=" text-4xl font-bold">{blog.name}</h1>
+        <p className=" text-2xl font-semibold"> {blog.description}</p>
+      </div>
+
+      <div className="profileDescrip flex justify-between items-center">
+        <h2 className="font-bold flex flex-col text-[1rem] ">
+          <span className="font-light opacity-70 ">author</span>
+          {blog.author.name}
+        </h2>
+        <p className="font-bold flex flex-col text-[1rem]">
+          <span className="font-light opacity-70 ">email</span>
+          {blog.author.email}
+        </p>
+        <p className="font-bold flex flex-col text-[1rem]">
+          <span className="font-light opacity-70 ">bio</span>
+          {blog.author.bio}
+        </p>
+        <p className="font-bold flex flex-col text-[1rem]">
+          <span className="font-light opacity-70 ">twitter</span>
+          {blog.author.social.twitter}
+        </p>
+        <p className="font-bold flex flex-col  text-[1rem]">
+          <span className="font-light opacity-70 ">linkedin</span>
+          {blog.author.social.linkedin}
+        </p>
+      </div>
+      <h2  className=" text-3xl font-bold"   >Posts:</h2>
+      <div className="postContainer flex justify-around ">
+        {blog.posts.map((item) => {
+          return (
+            <div className=" border rounded-2xl w-1/3 border-[#4F98CA] flex flex-col justify-between gap-1.5 p-1.5 h-[285px] overflow-y-scroll no-scrollbar  ">
+              <h2 className="font-bold text-[24px]"> {item.title} </h2>
+              <p> {item.content} </p>
+              <div className="tagContainer flex gap-1">
+                {item.tags.map((tag) => (
+                  <span className="bg-gray-500/70 text-[#EFFFFB]/70 rounded-[5px] p-1 text-[12px]">
+                    {" "}
+                    #{tag}{" "}
+                  </span>
+                ))}
+              </div>
+              <button
+                onClick={handelComment}
+                className=" cursor-pointer rounded-[10px] border-[#50D890] bg-[#50D890] text-[#EFFFFB] w-[35%] h-[40px] flex justify-center items-center  p-1 "
+              >
+                Comments {item.comments.length}
+              </button>
+              <span className="self-end-safe ml-1.5 opacity-40">
+                {" "}
+                {item.date}{" "}
+              </span>
+
+              {item.comments.map((comment) => {
+                return (
+                  <div
+                    className={`${
+                      commentDisplay
+                        ? "hidden"
+                        : "comments border border-[#50D890] p-1 mt-1.5 flex flex-col rounded-[10px]"
+                    } `}
+                  >
+                    <h3 className="font-bold flex flex-col text-[1rem]">
+                      <span className="font-light opacity-70 ">
+                        comment author
+                      </span>
+                      {comment.author}
+                    </h3>
+                    
+                    <p> {comment.text} </p>
+
+                    <span className="self-end-safe opacity-40">
+                      {" "}
+                      {comment.date}{" "}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+
+      <p>Page views: {blog.analytics.pageViews} </p>
+
+      <p>Average session duration: {blog.analytics.averageSessionDuration} </p>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,28 @@
 import Link from "next/link";
-import blogsData from "../data/blogs.json"
-
+import blogsData from "../data/blogs.json";
 
 export default function Home() {
   return (
-    <div className="felx"  >
-      {blogsData.map((item) => {
-        return (
-          <div className=" border border-cyan-100 w-[49%] p-2" key={item.id} >
-            <h2>{item.name}</h2>
-            <p className="text-[12px]" >{item.description}</p>
-            <Link href={`/${item.id}`} >learn more ...</Link>
-          </div>
-        );
-      })}
+    <div className="flex flex-col gap-4  p-7">
+      <h1 className="text-4xl font-bold" >Blogs List</h1>
+      <div className="listContainer justify-between flex flex-wrap gap-5  ">
+        {blogsData.map((item) => {
+          return (
+            <div
+              className=" border border-[#4F98CA] w-[30%] h-[25vh] p-3 rounded-2xl flex flex-col justify-around"
+              key={item.id}
+            >
+              <h2 className=" text-2xl">{item.name}</h2>
+              <p className="text-[12px]">{item.description}</p>
+              <Link href={`/${item.id}`}>
+                <div className=" border rounded-[10px] border-[#50D890] bg-[#50D890] text-[#EFFFFB] w-[45%] h-[40px] flex justify-center items-center  p-1 ">
+                  learn more ...
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
